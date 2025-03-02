98bacc5d9dcb8fe646a401943515314aca57c205
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-02 23:47:09 +0900

    :sparkles: feat: Column, CircularProgressIndicator Widget 추가

--- 2_splash_screen/lib/main.dart
+++ 2_splash_screen/lib/main.dart
@@ -5,7 +5,18 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0xFFC90481),
-        body: Center(child: Image.asset('assets/images/test_logo.png')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 24,
+          children: [
+            Center(child: Image.asset('assets/images/test_logo.png')),
+            Text(
+              'Splash Screen Project',
+              style: TextStyle(color: Colors.white),
+            ),
+            CircularProgressIndicator(color: Colors.white),
+          ],
+        ),
       ),
     ),
   );
