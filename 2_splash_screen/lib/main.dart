5aee6d8805576c496de0786b2d267eea05474ca1
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-02 23:54:45 +0900

    :recycle: refactor: HomeScreen widget 분리
    - StatelessWidget 사용해보기

--- 2_splash_screen/lib/main.dart
+++ 2_splash_screen/lib/main.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xFFC90481),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 24,
-          children: [
-            Center(child: Image.asset('assets/images/test_logo.png')),
-            Text(
-              'Splash Screen Project',
-              style: TextStyle(color: Colors.white),
-            ),
-            CircularProgressIndicator(color: Colors.white),
-          ],
-        ),
+  runApp(MaterialApp(home: HomeScreen()));
+}
+
+// StatelessWidget
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFC90481),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 24,
+        children: [
+          Center(child: Image.asset('assets/images/test_logo.png')),
+          Text('Splash Screen Project', style: TextStyle(color: Colors.white)),
+          CircularProgressIndicator(color: Colors.white),
+        ],
       ),
-    ),
-  );
+    );
+  }
 }
