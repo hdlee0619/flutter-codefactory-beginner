f5f4bc4cdf02c0fa60eb54f99f84164535d09445
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-04 00:43:12 +0900

    :sparkles: feat: 이미지 캐러셀 자동 넘김 기능 추가

--- 6_image_carousel/lib/screen/home_screen.dart
+++ 6_image_carousel/lib/screen/home_screen.dart
@@ -1,7 +1,37 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController controller = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(Duration(seconds: 2), (timer) {
+      int currentPage = controller.page!.toInt();
+      int nextPage = currentPage + 1;
+
+      if (nextPage == images.length) {
+        nextPage = 0;
+      }
+
+      controller.animateToPage(
+        nextPage,
+        duration: Duration(milliseconds: 500),
+        curve: Curves.linear,
+      );
+    });
+  }
 
   final List<String> images = [
     'assets/images/image0.png',
@@ -13,8 +43,20 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        controller: controller,
         children: images.map((image) => Image.asset(image)).toList(),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    if (timer != null) {
+      timer!.cancel();
+    }
+
+    controller.dispose();
+
+    super.dispose();
   }
 }
