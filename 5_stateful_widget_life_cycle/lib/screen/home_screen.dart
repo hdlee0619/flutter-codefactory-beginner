757879e93a57b3408cac053d2a518a7c73431a6d
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-03 23:59:54 +0900

    :sparkles: feat: Life Cycle 로깅

--- 5_stateful_widget_life_cycle/lib/screen/home_screen.dart
+++ 5_stateful_widget_life_cycle/lib/screen/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   bool _show = false;
+  Color color = Colors.red;
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +19,15 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            if (_show) TestScreen(),
+            if (_show)
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    color = color == Colors.red ? Colors.blue : Colors.red;
+                  });
+                },
+                child: TestScreen(color: color),
+              ),
             SizedBox(height: 32.0),
             ElevatedButton(
               onPressed: () {
@@ -35,11 +44,61 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class TestScreen extends StatelessWidget {
-  const TestScreen({super.key});
+class TestScreen extends StatefulWidget {
+  TestScreen({super.key, required this.color}) {
+    print('1) Stateful Widget constructor');
+  }
+
+  final Color color;
+
+  @override
+  State<TestScreen> createState() {
+    print('2) Stateful Widget createState()');
+
+    return _TestScreenState();
+  }
+}
+
+class _TestScreenState extends State<TestScreen> {
+  @override
+  void initState() {
+    print('3) Stateful Widget initState()');
+
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('4) Stateful Widget didChangeDependencies()');
+
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(covariant TestScreen oldWidget) {
+    print('7) Stateful Widget didUpdateWidget()');
+
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(width: 50.0, height: 50.0, color: Colors.red);
+    print('5) Stateful Widget build()');
+
+    return Container(width: 50.0, height: 50.0, color: widget.color);
+  }
+
+  @override
+  void deactivate() {
+    print('6) Stateful Widget deactivate()');
+
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('7) Stateful Widget dispose()');
+
+    super.dispose();
   }
 }
