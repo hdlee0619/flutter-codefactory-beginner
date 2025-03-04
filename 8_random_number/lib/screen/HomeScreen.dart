c1eafffaa65da6bf69cb6e413ed88c3e12695060
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-04 15:55:13 +0900

    :lipstick: design: UI 디자인 적용

--- 8_random_number/lib/screen/HomeScreen.dart
+++ 8_random_number/lib/screen/HomeScreen.dart
@@ -1,10 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:random_number/const/color.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [_Header(), _Body(), _Footer()],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '랜덤 숫자 생성기',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.settings),
+          color: redColor,
+        ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Text('''
+              123
+              456
+              789
+              '''),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  const _Footer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: redColor,
+        foregroundColor: Colors.white,
+      ),
+      child: Text('생성하기'),
+    );
   }
 }
