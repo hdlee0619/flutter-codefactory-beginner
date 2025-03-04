c3a82411296d45a9b0c723b594b052345a4589d9
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-05 01:52:08 +0900

    :sparkles: feat: 출석 체크 기능 추가

--- 10_check_attendance/lib/screen/home_screen.dart
+++ 10_check_attendance/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -10,6 +11,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isCheckIn = false;
+  bool canCheckIn = false;
+
+  final double companyDistance = 100;
+
   final CameraPosition initialPosition = CameraPosition(
     target: LatLng(37.5214, 126.924652),
     zoom: 15,
@@ -35,6 +41,31 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    Geolocator.getPositionStream().listen((event) {
+      final start = LatLng(37.5214, 126.924652);
+      final end = LatLng(event.latitude, event.longitude);
+
+      final distance = Geolocator.distanceBetween(
+        start.latitude,
+        start.longitude,
+        end.latitude,
+        end.longitude,
+      );
+
+      setState(() {
+        if (distance > companyDistance) {
+          canCheckIn = false;
+        } else {
+          canCheckIn = true;
+        }
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -56,6 +87,7 @@ class _HomeScreenState extends State<HomeScreen> {
           return Column(
             children: [
               Expanded(
+                flex: 3,
                 child: GoogleMap(
                   onMapCreated: (GoogleMapController controller) {
                     this.controller = controller;
@@ -65,6 +97,50 @@ class _HomeScreenState extends State<HomeScreen> {
                   myLocationButtonEnabled: false,
                   myLocationEnabled: true,
                   zoomControlsEnabled: false,
+                  markers: {
+                    Marker(
+                      markerId: MarkerId('123'),
+                      position: LatLng(37.5214, 126.9246),
+                    ),
+                  },
+                  circles: {
+                    Circle(
+                      circleId: CircleId('inDistance'),
+                      center: LatLng(37.5214, 126.9246),
+                      radius: companyDistance,
+                      fillColor:
+                          canCheckIn
+                              ? Colors.blue.withAlpha(100)
+                              : Colors.red.withAlpha(100),
+                      strokeColor: canCheckIn ? Colors.blue : Colors.red,
+                      strokeWidth: 1,
+                    ),
+                  },
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.white,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        isCheckIn ? Icons.check : Icons.timelapse_outlined,
+                        color: isCheckIn ? Colors.green : Colors.blue,
+                      ),
+                      SizedBox(height: 16.0),
+                      if (!isCheckIn && canCheckIn)
+                        CupertinoButton(
+                          onPressed: handleCheckIn,
+                          child: Text(
+                            '출근하기',
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                    ],
+                  ),
                 ),
               ),
             ],
@@ -82,5 +158,37 @@ class _HomeScreenState extends State<HomeScreen> {
     controller.animateCamera(
       CameraUpdate.newLatLng(LatLng(location.latitude, location.longitude)),
     );
+  }
+
+  handleCheckIn() async {
+    final result = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+          title: Text('출근하기'),
+          content: Text('출근을 하시겠습니까?'),
+          actions: [
+            CupertinoButton(
+              child: Text('취소', style: TextStyle(color: Colors.red)),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+            CupertinoButton(
+              child: Text('승인', style: TextStyle(color: Colors.blue)),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    if (result) {
+      setState(() {
+        isCheckIn = true;
+      });
+    }
   }
 }
