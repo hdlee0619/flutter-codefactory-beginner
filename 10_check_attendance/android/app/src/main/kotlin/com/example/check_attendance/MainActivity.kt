45218a623889808210fb8306e913d9b8a8a99693
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-04 21:27:51 +0900

    :tada: init: check attendance 프로젝트

---
+++ 10_check_attendance/android/app/src/main/kotlin/com/example/check_attendance/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.check_attendance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ 10_check_attendance/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:check_attendance/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomeScreen());
+}
