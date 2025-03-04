6d0ebae0d7794ac5c1bcb7e6625bc7c58dc583ac
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-05 01:15:05 +0900

    :sparkles: feat: 현재 위치 렌더링, 현재 위치로 이동하기 기능 추가

--- 10_check_attendance/lib/screen/home_screen.dart
+++ 10_check_attendance/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,12 +15,72 @@ class _HomeScreenState extends State<HomeScreen> {
     zoom: 15,
   );
 
+  checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!isLocationEnabled) {
+      throw Exception('위치 기능을 활성화 해주세요.');
+    }
+
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if (checkedPermission == LocationPermission.denied) {
+      checkedPermission = await Geolocator.requestPermission();
+    }
+
+    if (checkedPermission != LocationPermission.always &&
+        checkedPermission != LocationPermission.whileInUse) {
+      throw Exception('위치 권한이 없습니다.');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(child: GoogleMap(initialCameraPosition: initialPosition)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('오늘도 출근'),
+        actions: [
+          IconButton(
+            onPressed: handleMyLocation,
+            icon: Icon(Icons.my_location),
+          ),
+        ],
+      ),
+      body: FutureBuilder(
+        future: checkPermission(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
+
+          return Column(
+            children: [
+              Expanded(
+                child: GoogleMap(
+                  onMapCreated: (GoogleMapController controller) {
+                    this.controller = controller;
+                  },
+                  initialCameraPosition: initialPosition,
+                  mapType: MapType.normal,
+                  myLocationButtonEnabled: false,
+                  myLocationEnabled: true,
+                  zoomControlsEnabled: false,
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  late final GoogleMapController controller;
+
+  handleMyLocation() async {
+    final location = await Geolocator.getCurrentPosition();
+
+    controller.animateCamera(
+      CameraUpdate.newLatLng(LatLng(location.latitude, location.longitude)),
     );
   }
 }
