f5df83ba1c57c76cecec367bed5c38e4c75a73ad
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-05 03:08:36 +0900

    :lipstick: design: home_screen UI 추가

--- 11_video_call/lib/main.dart
+++ 11_video_call/lib/main.dart
@@ -3,5 +3,7 @@ import 'package:flutter/material.dart';
 import 'screen/home_screen.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomeScreen()));
+  runApp(
+    MaterialApp(theme: ThemeData(fontFamily: 'NotoSans'), home: HomeScreen()),
+  );
 }
--- 11_video_call/lib/screen/home_screen.dart
+++ 11_video_call/lib/screen/home_screen.dart
@@ -5,6 +5,76 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      body: Column(children: [_Logo(), _MainImage(), _JoinButton()]),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(16.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.blue[300]!,
+                blurRadius: 12.0,
+                spreadRadius: 3.0,
+              ),
+            ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.videocam, color: Colors.white),
+                SizedBox(width: 12.0),
+                Text(
+                  "LIVE",
+                  style: TextStyle(color: Colors.white, fontSize: 30.0),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _MainImage extends StatelessWidget {
+  const _MainImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(child: Image.asset('assets/images/home_img.png')),
+    );
+  }
+}
+
+class _JoinButton extends StatelessWidget {
+  const _JoinButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+          child: Text('입장하기', style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
   }
 }
