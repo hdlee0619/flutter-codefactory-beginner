f6b68e234e36c88863b20710440a0a359912e855
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-04 13:20:30 +0900

    :lipstick: design: 디자인 구성 추가

--- 7_u_and_i/lib/screen/home_screen.dart
+++ 7_u_and_i/lib/screen/home_screen.dart
@@ -5,6 +5,36 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.pink[200],
+      body: SafeArea(
+        bottom: false,
+        child: SizedBox(
+          // 현재 실행하고 있는 디바이스의 가로 길이
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  child: Column(
+                    children: [
+                      Text('U&I'),
+                      Text("우리 처음 만난 날"),
+                      IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
+                      Text('D + 1'),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: Image.asset('assets/images/middle_image.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
