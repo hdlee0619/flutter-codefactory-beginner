918075c0eab519c3c4c99e30bc9fc7a4fb456aa9
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-04 14:40:20 +0900

    :sparkles: feat: 날짜 선택 , 계산기능 추가

--- 7_u_and_i/lib/screen/home_screen.dart
+++ 7_u_and_i/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -19,11 +20,19 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class _Top extends StatelessWidget {
+class _Top extends StatefulWidget {
   const _Top({super.key});
 
   @override
+  State<_Top> createState() => _TopState();
+}
+
+class _TopState extends State<_Top> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
   Widget build(BuildContext context) {
+    final now = DateTime.now();
     final textTheme = Theme.of(context).textTheme;
 
     return Expanded(
@@ -32,14 +41,44 @@ class _Top extends StatelessWidget {
           children: [
             Text('U&I', style: textTheme.displayLarge),
             Text('우리 처음 만난 날', style: textTheme.bodyLarge),
-            Text('2022-11-11', style: textTheme.bodyMedium),
+            Text(
+              '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+              style: textTheme.bodyMedium,
+            ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                showCupertinoDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        color: Colors.white,
+                        height: 300.0,
+                        child: CupertinoDatePicker(
+                          mode: CupertinoDatePickerMode.date,
+                          dateOrder: DatePickerDateOrder.ymd,
+
+                          onDateTimeChanged: (DateTime date) {
+                            setState(() {
+                              selectedDate = date;
+                            });
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                  barrierDismissible: true,
+                );
+              },
               icon: Icon(Icons.favorite),
               iconSize: 60.0,
               color: Colors.red,
             ),
-            Text('D + 1', style: textTheme.displayMedium),
+            Text(
+              'D + ${now.difference(selectedDate).inDays + 1}',
+              style: textTheme.displayMedium,
+            ),
           ],
         ),
       ),
