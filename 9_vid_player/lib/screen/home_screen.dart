f36af3d5e6528b501b669e8a18a0fc2438ce00bc
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-04 19:40:20 +0900

    :sparkles: feat: 로고 클릭 시 조건부 렌더링 기능 추가

--- 9_vid_player/lib/screen/home_screen.dart
+++ 9_vid_player/lib/screen/home_screen.dart
@@ -1,34 +1,43 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool showVideoPlayer = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFF2A3A7C), Color(0xFF000118)],
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [_Logo(), SizedBox(height: 24.0), _Title()],
-        ),
-      ),
+      body:
+          showVideoPlayer
+              ? _VideoPlayer()
+              : _VideoSelector(onTap: handleLogoTap),
     );
+  }
+
+  handleLogoTap() {
+    setState(() {
+      showVideoPlayer = true;
+    });
   }
 }
 
 class _Logo extends StatelessWidget {
-  const _Logo({super.key});
+  final VoidCallback onTap;
+
+  const _Logo({required this.onTap, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset('assets/images/logo.png');
+    return GestureDetector(
+      onTap: onTap,
+      child: Image.asset('assets/images/logo.png'),
+    );
   }
 }
 
@@ -49,6 +58,38 @@ class _Title extends StatelessWidget {
         Text("VIDEO", style: textStyle),
         Text("PLAYER", style: textStyle.copyWith(fontWeight: FontWeight.w700)),
       ],
+    );
+  }
+}
+
+class _VideoPlayer extends StatelessWidget {
+  const _VideoPlayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Center(child: Text('TEST')));
+  }
+}
+
+class _VideoSelector extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const _VideoSelector({required this.onTap, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xFF2A3A7C), Color(0xFF000118)],
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [_Logo(onTap: onTap), SizedBox(height: 24.0), _Title()],
+      ),
     );
   }
 }
