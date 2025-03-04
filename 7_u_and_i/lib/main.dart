09a2eea57973df14dd8838880a8eed680a8254a8
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-04 13:40:33 +0900

    :art: refactor: TextTheme 생성, 적용

--- 7_u_and_i/lib/main.dart
+++ 7_u_and_i/lib/main.dart
@@ -3,5 +3,35 @@ import 'package:flutter/material.dart';
 import 'screen/home_screen.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomeScreen()));
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+            color: Colors.white,
+            fontFamily: 'parisienne',
+            fontSize: 80.0,
+          ),
+          displayMedium: TextStyle(
+            color: Colors.white,
+            fontFamily: 'sunflower',
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyLarge: TextStyle(
+            color: Colors.white,
+            fontFamily: 'sunflower',
+            fontSize: 30.0,
+          ),
+          bodyMedium: TextStyle(
+            color: Colors.white,
+            fontFamily: 'sunflower',
+            fontSize: 20.0,
+          ),
+        ),
+      ),
+    ),
+  );
 }
--- 7_u_and_i/lib/screen/home_screen.dart
+++ 7_u_and_i/lib/screen/home_screen.dart
@@ -24,49 +24,22 @@ class _Top extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return Expanded(
       child: Container(
         child: Column(
           children: [
-            Text(
-              'U&I',
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'parisienne',
-                fontSize: 80.0,
-              ),
-            ),
-            Text(
-              '우리 처음 만난 날',
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'sunflower',
-                fontSize: 30.0,
-              ),
-            ),
-            Text(
-              '2022-11-11',
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'sunflower',
-                fontSize: 20.0,
-              ),
-            ),
+            Text('U&I', style: textTheme.displayLarge),
+            Text('우리 처음 만난 날', style: textTheme.bodyLarge),
+            Text('2022-11-11', style: textTheme.bodyMedium),
             IconButton(
               onPressed: () {},
               icon: Icon(Icons.favorite),
               iconSize: 60.0,
               color: Colors.red,
             ),
-            Text(
-              'D + 1',
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'sunflower',
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
+            Text('D + 1', style: textTheme.displayMedium),
           ],
         ),
       ),
