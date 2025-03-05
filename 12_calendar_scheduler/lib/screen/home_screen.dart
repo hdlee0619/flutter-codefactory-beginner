92255d3824ac83b56e089708f1ded6c132df2606
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-05 13:53:03 +0900

    :sparkles: 일정 위젯 추가

--- 12_calendar_scheduler/lib/screen/home_screen.dart
+++ 12_calendar_scheduler/lib/screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:calendar_scheduler/components/calender.dart';
+import 'package:calendar_scheduler/components/schedule_card.dart';
+import 'package:calendar_scheduler/components/today_banner.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,7 +11,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  DateTime? selectedDay;
+  DateTime selectedDay = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +27,26 @@ class _HomeScreenState extends State<HomeScreen> {
               focusedDay: DateTime(2025, 3, 5),
               onDaySelected: handleDaySelected,
               selectedDayPredicate: selectedDayPredicate,
+            ),
+            TodayBanner(selectedDay: selectedDay, taskCount: 0),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 16.0,
+                  right: 16.0,
+                  top: 16.0,
+                ),
+                child: ListView(
+                  children: [
+                    ScheduleCard(
+                      startTime: DateTime(2025, 3, 5, 12),
+                      endTime: DateTime(2025, 3, 5, 13),
+                      content: '점심 먹기',
+                      color: Colors.blue,
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
