da9a911abbfdd789f0c906dc4a4b19035d517047
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-03-05 13:17:24 +0900

    :sparkles: feat: 캘린더 한국어 적용

--- 12_calendar_scheduler/lib/screen/home_screen.dart
+++ 12_calendar_scheduler/lib/screen/home_screen.dart
@@ -27,6 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: SafeArea(
         child: TableCalendar(
+          locale: 'ko_KR',
           focusedDay: DateTime(2025, 3, 5),
           firstDay: DateTime(1800),
           lastDay: DateTime(3000),
